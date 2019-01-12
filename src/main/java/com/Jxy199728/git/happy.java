5b40409bfe9256293cd22ff4f6eded34a83aa1d2
eclipse_user <eclipse_user@bt21.com>
2019-01-13 03:56:03 +0800

    jin & jimin

--- src/main/java/com/Jxy199728/git/happy.java
+++ src/main/java/com/Jxy199728/git/happy.java
@@ -2,8 +2,8 @@ package com.Jxy199728.git;
 
 public class happy {
 	public static void main(String[] args){
-		System.out.println("Test ...");
-		System.out.println("Test2 ...");
+		System.out.println("jin ...");
+		System.out.println("jimin ...");
 
 	}
 
